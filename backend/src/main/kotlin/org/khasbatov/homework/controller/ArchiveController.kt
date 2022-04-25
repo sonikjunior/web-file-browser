3160e9d8066bdcbf8037e00d9c7df3e77f81c0da
Ruslan Khasbatov <r.khasbatov@tinkoff.ru>
2022-04-25 11:40:11 +0300

    [backend] Permitting cross-origin requests from frontend was added

--- backend/src/main/kotlin/org/khasbatov/homework/controller/ArchiveController.kt
+++ backend/src/main/kotlin/org/khasbatov/homework/controller/ArchiveController.kt
@@ -4,8 +4,10 @@ import org.khasbatov.homework.model.FileRequestDto
 import org.khasbatov.homework.model.FileResponseDto
 import org.khasbatov.homework.service.ArchiveService
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.RestController
 
+@CrossOrigin(origins = ["http://localhost:3000/"])
 @RestController
 class ArchiveController(
     private val archiveService: ArchiveService
--- backend/src/main/kotlin/org/khasbatov/homework/controller/FolderController.kt
+++ backend/src/main/kotlin/org/khasbatov/homework/controller/FolderController.kt
@@ -4,8 +4,10 @@ import org.khasbatov.homework.model.FileRequestDto
 import org.khasbatov.homework.model.FileResponseDto
 import org.khasbatov.homework.service.FolderService
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.RestController
 
+@CrossOrigin(origins = ["http://localhost:3000/"])
 @RestController
 class FolderController(
     private val folderService: FolderService
